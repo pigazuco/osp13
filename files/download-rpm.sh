a490f5ea4fd418cdb987097760813581fd1d9110
Pigazuco <45468172+pigazuco@users.noreply.github.com>
2020-04-28 08:46:08 +0200

    Update download-rpm.sh

--- files/download-rpm.sh
+++ files/download-rpm.sh
@@ -5,5 +5,6 @@ do
         wget_output=$(yumdownloader --destdir=/tmp/rpms/ $line)
         if [ $? -ne 0 ];then
                 echo "File $line was not found"
+                echo "$line" >> missing.txt
         fi
 done
